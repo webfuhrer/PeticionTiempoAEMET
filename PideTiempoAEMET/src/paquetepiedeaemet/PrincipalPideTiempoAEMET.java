ed62b863db3d7204d6641f5e05ff9f124e4c8053
Mañanas <Maanas@DESKTOP-FO1NDT3>
2018-10-30 13:44:42 +0100

    commit2

--- PideTiempoAEMET/src/paquetepiedeaemet/PrincipalPideTiempoAEMET.java
+++ PideTiempoAEMET/src/paquetepiedeaemet/PrincipalPideTiempoAEMET.java
@@ -1,15 +1,39 @@
 package paquetepiedeaemet;
 
-import org.jdom2.Document;
+//import org.jdom2.Document;
 
 public class PrincipalPideTiempoAEMET {
 
 	public static void main(String[] args) {
-		String codigo=EntradaSalida.pedirCodigoMunicipio();
+		/*ArrayList<Provincia> lista_provincias=AccesoBD.devoverProvincias();//
+		int id_provincia=EntradaSalida.mostrarMenu(lista_provincias);
+		ArrayList<Municipio> lista_municipios=AccesoBD.devolverMunicipiosPorProvincia(id_provincia);//SELECT ... Where
+		int cod_municipio=Entradasalida.mostrarMunicipios(lista_municipios);
+		*/
+		
+		int id_provincia=28;
+		int cod_municipio=8;
+		String provincia="";
+		String municipio="";
+		
+		if (id_provincia<10)
+		{
+			provincia="0"+id_provincia;
+		}
+		
+		municipio=String.valueOf(cod_municipio);
+		while(municipio.length()<3)
+		{
+			municipio="0"+municipio;
+		}
+		
+		String codigo=provincia+municipio;
+		System.out.println(codigo);
+		/*String codigo=EntradaSalida.pedirCodigoMunicipio();
 		String xml=AccesoWeb.pedirXML(codigo);
 		Document documento_xml=ParseaXML.convertirStringEnDocument(xml);
 		Clima c=ParseaXML.parsearXML(documento_xml);
-		EntradaSalida.mostrarPrediccion(c);
+		EntradaSalida.mostrarPrediccion(c);*/
 	}
 
 }
